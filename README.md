ada1228f53ec43bdf23af3f8006244fc214813fe
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-01-10 21:02:04 +0000

    Updated ReadMe File

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Password-_Generator
-You'll create an app that runs in the browser and features dynamically updated HTML and CSS, all powered by JavaScript code that you will write. Your app will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+You'll create an app that runs in the browser and features dynamically updated HTML and CSS, all powered by **JavaScript code** that you will write. Your app will have a **clean and polished user interface that is responsive,** ensuring that it adapts to multiple screen sizes.
+
+
+
+## User Story
+
+- **AS** a user
+- **I WANT** to create a password generator
+- **SO THAT** an employee can generate a random password to their specification
+
+## Acceptance Criteria
+
+- **WHEN** viewing the password length prompt
+- **THEN** the user can choose the password length of between 10 and 64 characters
+- **WHEN** viewing the 'include numeric characters' prompt
+- **THEN** the user can choose if they want to include them in their password
+- **WHEN** viewing the 'include lowercase letters' prompt
+- **THEN** the user can choose if they want to include them in their password
+- **WHEN** viewing the 'include uppercase letters' prompt
+- **THEN** the user can choose if they want to include them in their password
+- **WHEN** viewing the 'include special characters' prompt
+- **THEN** the user can choose if they want to include them in their password
+
+## Usage
+
+![alt text](.)
+
+## Credits
+The following tutorials where used in this project:
+
+https://www.youtube.com/watch?v=JLZ4FH-L4eI
+https://www.youtube.com/watch?v=Pgdvj6InRKI&list=PLgBH1CvjOA62PBFIDq55-S6Beivje30A2&index=15
+https://www.youtube.com/watch?v=9hin-2yeCWY&list=PLgBH1CvjOA62PBFIDq55-S6Beivje30A2&index=15
+https://www.youtube.com/watch?v=_zYb4WB1eDo&list=PLgBH1CvjOA62PBFIDq55-S6Beivje30A2&index=17
+https://unitstep.net/blog/2009/08/11/evaluation-of-boolean-values-in-javascript/
+
+
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
--- starter/script.js
+++ starter/script.js
@@ -93,61 +93,67 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 // function getPasswordOptions() {
+  // var password = //create a new array and push numbers/characters to the new array, then I need to randomly select a combination of numbers based on the UserChoice length
+  
+  let passwordLength = userChoice
   var userChoice=prompt ("How many characters would you like your password to contain?");
-  // console.log(userChoice)
-  if (userChoice > 0 && userChoice < 64 ) {
-    alert ("Great Choice!")
-    } 
-    else alert("Your password must contain between 10 and 64 characters. Please try again.") {
-      //need to repeat prompt here
-    }
+  if (userChoice > 0 && userChoice < 64) {
+    console.log(userChoice)
+  } 
+  else if (userChoice < 0 || userChoice > 64) {
+  alert("Your password must contain between 10 and 64 characters. Please try again.")
+    prompt ("How many characters would you like your password to contain?"); //need to have a condition that if the user doesn't select anything then they need to be prompted to choose a password
+  } //it needs to loop until user chooses the correct length - at the moment it is not
 
+
+let password = "" //need to store random characters to this striing and then ensure they are the same length as the userchoice
   //How many characters would you like your password to contain? 10-64
       //if less than 10, alert user "You password must contain 10 or more characters"
       //if more than 64, alert user "Your password cannot contain more than 64 characters"
 
 // Would you like your password to contain lowercase letters?
-confirm ("Would you like your password to contain lowercase letters?");
-console.log(userChoice)
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+var userChoice1 = confirm ("Would you like your password to contain lowercase letters?");
+if (userChoice1 % 2 === 0) { //this isn't working - how can I evaluate user answer to tru to generate random numbers?
+ console.log(getRandom(lowerCasedCharacters))
+}
+
+// console.log("This is the password", password)
 
   //if ok, generate random lowercase letters from lowerCasedCharacters // if canceldo not generate lowercase letters
 
 // Would you like your password to contain uppercase letters?
-confirm ("Would you like your password to contain uppercase letters?");
-console.log(userChoice)
+var userChoice2 = confirm ("Would you like your password to contain uppercase letters?");
+console.log(userChoice2)
 
   //if ok, generate random uppercase letters from upperCasedCharacters // if cancel do not generate uppercase letters
 
 // Would you like your password to contain numbers?
-confirm ("Would you like your password to contain numbers?");
-console.log(userChoice)
+var userChoice3 = confirm ("Would you like your password to contain numbers?");
+console.log(userChoice3)
 
   //if ok, generate random numbers from numericCharacters // if cancel do not generate numbers
 
 
 // Would you like your password to contain special characters ($@%&*, etc)?
-confirm ("Would you like your password to contain special characters?");
-console.log(userChoice)
+var userChoice4 = confirm ("Would you like your password to contain special characters?");
+console.log(userChoice4)
 
   //if ok, generate random special characters from specialCharacters // if cancel do not generate special characters
 
 // }
 
 // Function for getting a random element from an array
-// function getRandom(arr) {
-//   var randomInd = Math.floor(Math.random() * arr.length);
-//   var randomEle = arr[randomInd]
-//   return randomEle;
-// }
 
 
-function getRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-console.log(getRandom(numericCharacters))
-console.log(getRandom(specialCharacters))
-console.log(getRandom(upperCasedCharacters))
-console.log(getRandom(lowerCasedCharacters))
+
+
+// console.log(getRandom(numericCharacters))
+// console.log(getRandom(specialCharacters))
+// console.log(getRandom(upperCasedCharacters))
+// console.log(getRandom(lowerCasedCharacters))
 
   
 
