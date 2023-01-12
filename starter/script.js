edb499027e90bb11a3569be7ef397085eb049a9e
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-01-12 13:56:36 +0000

    Got ok and cancel working correctly

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,16 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+}
+passwordText.value = password;
+
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
 
 
 
@@ -13,97 +26,93 @@ var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
 's','t','u','v','w','x','y','z'];
 
 // // Array of uppercase characters to be included in password
-var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
-'S','T','U','V','W','X','Y','Z'];
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
 
 // Function to prompt user for password options
-  let userChoice;
+var userChoice = " ";
 do {
      userChoice = prompt("How many characters would you like your password to contain (10-64)?");
 } while (userChoice < 10 || userChoice > 64) {
   // alert("Your password must contain between 10 and 64 characters. Please try again.")
 }
 
-// let password = "" //need to store random characters to this striing and then ensure they are the same length as the userchoice
-//   //How many characters would you like your password to contain? 10-64
-//       //if less than 10, alert user "You password must contain 10 or more characters"
-//       //if more than 64, alert user "Your password cannot contain more than 64 characters"
+
+// Loop if answer is outside the parameters 
+      // while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+      //   alert("You must choose at least one parameter");
+        
+        
+       
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
 
 // Would you like your password to contain lowercase letters?
 function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-var userChoice1 = confirm ("Would you like your password to contain lowercase letters?");
-if (userChoice1 % 2 === 0) { //this generates a letter on OK and Cancel
+
+if (confirmLowerCase === true) {
+  console.log(getRandom(lowerCasedCharacters));
+} else {
+  console.log("No character selected")
 }
-else (userChoice1 === null)
-console.log(getRandom(lowerCasedCharacters))
+
 
   //if ok, generate random lowercase letters from lowerCasedCharacters // if canceldo not generate lowercase letters
 
 // Would you like your password to contain uppercase letters?
-var userChoice2 = confirm ("Would you like your password to contain uppercase letters?");
-if (userChoice2 % 2 === 0) {
+
+if (confirmUpperCase === true) {
+  console.log(getRandom(upperCasedCharacters))
+} else {
+  console.log("No character selected")
 }
-console.log(getRandom(upperCasedCharacters))
+
 
   //if ok, generate random uppercase letters from upperCasedCharacters // if cancel do not generate uppercase letters
 
 // Would you like your password to contain numbers?
-var userChoice3 = confirm ("Would you like your password to contain numbers?");
-if (userChoice3 % 2 === 0) {
+if (confirmNumericCharacter === true) {
+  console.log(getRandom(numericCharacters))
+} else {
+  console.log("No character selected")
 }
-console.log(getRandom(numericCharacters))
-
 
 //   //if ok, generate random numbers from numericCharacters // if cancel do not generate numbers
 
-
 // // Would you like your password to contain special characters ($@%&*, etc)?
-var userChoice4 = confirm ("Would you like your password to contain special characters?");
-if (userChoice4 % 2 === 0) {
+if (confirmSpecialCharacter === true) {
+  console.log(getRandom(specialCharacters))
+} else {
+  console.log("No character selected")
 }
-console.log(getRandom(specialCharacters))
 
 //   //if ok, generate random special characters from specialCharacters // if cancel do not generate special characters
 
-// // }
-
-// // Function for getting a random element from an array
 
 
-
-
-// // console.log(getRandom(numericCharacters))
-// // console.log(getRandom(specialCharacters))
-// // console.log(getRandom(upperCasedCharacters))
-// // console.log(getRandom(lowerCasedCharacters))
-
-  
-
-// // }
 
 // // // Function to generate password with user input //this is where the password will generate
-// // function generatePassword() {
+// function generatePassword() {
+//   var randomPassword = ""
+// }
+//   for (var i = 0; i < confirmLength; i++) {
+//     randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+//     console.log(randomPassword)
+//   }
+//   return randomPassword;
 
-// // }
-
-// // // Get references to the #generate element
-// // var generateBtn = document.querySelector('#generate');
-
-// // // Write password to the #password input
-// // function writePassword() {
-// //   var password = generatePassword();
-// //   var passwordText = document.querySelector('#password');
-
-// //   passwordText.value = password;
-// // }
-
-// // // Add event listener to generate button
-// // generateBtn.addEventListener('click', writePassword);
 
 // // // * Generate a password when the button is clicked
 // // //   * Present a series of prompts for password criteria
 // // //     * 
 // // //   * Code should validate for each input and at least one character type should be selected
-// // //   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+;// // //   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
